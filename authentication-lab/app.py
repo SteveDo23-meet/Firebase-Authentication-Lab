a5775b05e3c761873bdb9cf10c5163b3ae738804
SteveDo23-meet <109904642+SteveDo23-meet@users.noreply.github.com>
2022-08-02 18:29:37 +0300

    also this is the RT database

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -3,17 +3,20 @@ from flask import session as login_session
 import pyrebase
 
 
-firebaseConfig = {
+config = {
   "apiKey": "AIzaSyBK31Rb887yZDPtfGxSFaWUIsOsnAuXQIQ",
   "authDomain": "tonal-shore-325713.firebaseapp.com",
   "projectId": "tonal-shore-325713",
   "storageBucket": "tonal-shore-325713.appspot.com",
   "messagingSenderId": "862702764406",
   "appId": "1:862702764406:web:6e819b95149cd473f9fa57",
-  "measurementId": "G-YRFB0KS86M"
-  "databaseurl" : ""
+  "measurementId": "G-YRFB0KS86M",
+  "databaseURL" : "https://tonal-shore-325713-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
@@ -22,52 +25,71 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 @app.route('/', methods=['GET', 'POST'])
 def signin():
     error = ""
-    if request.method == 'post':
+    if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-    try:
-        login_session['user'] = 
-            auth.sign_in_with_email_and_password(email , password)
-        return redirect(url_for('home'))
-    except :
-        error = "Authentication failed"
+        try:
+            login_session['user']=auth.sign_in_with_email_and_password(email , password)
+            return redirect(url_for('home'))
+        except :
+            error = "Authentication failed"
 
-return render_template("signin.html")
+    return render_template("signin.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     error = ""
-    if request.method == 'post':
+
+    if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-    try:
-        login_session['user'] = 
-            auth.create_user_with_email_and_password(email , password)
-        return redirect(url_for('add_tweet'))
-    except :
-        error = "Authentication failed"
+        username = request.form['username']
+        bio = request.form['bio']
+        full_name = request.form['full_name']
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email , password)
+            user = {"username": username, "email": email , "password" : password , "bio" : bio , "full_name" : full_name}
+            db.child("Users").child(login_session['user']['localId']).set(user)
+            return redirect(url_for('add_tweet'))
+        except :
+            error = "Authentication failed"
 
-return render_template("signup.html")
+    return render_template("signup.html")
 
-
+tweets= db.child("Tweets").get().val()
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
-    return render_template("add_tweet.html")
+    error = ""
+    if request.method == 'POST':
+        Title = request.form['Title']
+        Text = request.form['Text']
+        uid = login_session['user']['localId']
+        tweet = {"Title" : Title , "Text" : Text , "uid" : uid}
 
+        try:
+            db.child("Tweets").push(tweet)
+            return redirect(url_for("all_tweets"))
+        except :
+            error = "Could not add the tweet sorry"
+
+    return render_template("add_tweet.html")
 
 @app.route('/signout', methods=['GET', 'POST'])
 def signout():
-    login_session['user'] = None
-    auth.current_user = None
-    return redirect(url_for('signin'))
+    try:
+        login_session['user'] = None
+        auth.current_user = None
+        return redirect(url_for('signin'))
+    except:    
+        return render_template("signin.html")
 
-return render_template("signin.html")
+@app.route('/all_tweets' , methods=['GET' , 'POST'])
+def all_tweets():
+    tweets = db.child('Tweets').get().val()
+    return render_template('all_tweets.html' , Tweets = tweets)
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
 
-app = flask(__name__)
